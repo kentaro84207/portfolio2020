a60cd827924ce6b8ea234b21542bcac83e68d783
kentaro84207 <kentaro84207@gmail.com>
2020-01-25 16:49:07 +0900

    Add transition animetion

--- src/animation/subEntry.js
+++ src/animation/subEntry.js
@@ -1,4 +1,4 @@
-import { gsap, Power3, Power4 } from 'gsap'
+import { gsap, Power3, Power4, Back } from 'gsap'
 import { colors } from "../constants/constants"
 
 const subEntry = () => {
@@ -10,7 +10,7 @@ const subEntry = () => {
     ease: Power3.easeInOut,
   })
   gsap.from(".an-link", {
-    y: -20,
+    y: -10,
     opacity: 0,
     duration: 0.9,
     ease: Power3.easeInOut,
@@ -24,6 +24,13 @@ const subEntry = () => {
   gsap.from(".an-bg", {
     backgroundColor: colors.primaryColor,
     ease: Power4.easeInOut,
+  })
+  gsap.from(".an-subPage", {
+    y: 20,
+    opacity: 0,
+    duration: 1.4,
+    ease: Back.easeOut.config(4),
+    stagger: 0.1,
   })
   bgTl
     .to(".an-naviBg", {
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,28 +8,32 @@ const AboutPage = () => {
   return (
     <Layout>
       <SEO title="About" />
-      <h1>About me</h1>
-      <p css={mt20}>
+      <h1 className="an-subPage">About me</h1>
+      <p className="an-subPage" css={mt20}>
         I am Kentaro Suzuki, a web developer who likes design and coding.
         <br />
         Currently producing web application using TypeScript and Ruby on Rails at tech company.
       </p>
-      <h2 css={mt50}>Skill set</h2>
-      <ul css={mt20}>
-        <li>HTML / CSS / SCSS / GSAP</li>
-        <li>JavaScript/ TypeScript / React / Gatsby / Vue / Nuxt</li>
-        <li>Ruby / Ruby on Rails</li>
-        <li>WordPress / Contentful</li>
-        <li>AWS / GCP / Netlify / Firebase</li>
-        <li>Git / GitHub / CircleCI</li>
-        <li>etc...</li>
-      </ul>
-      <h2 css={mt50}>Social Media</h2>
-      <ul css={mt20}>
-        <li>GitHub</li>
-        <li>Wantedly</li>
-        <li>Twitter</li>
-      </ul>
+      <section className="an-subPage">
+        <h2 css={mt50}>Skill set</h2>
+        <ul css={mt20}>
+          <li>HTML / CSS / SCSS / GSAP</li>
+          <li>JavaScript/ TypeScript / React / Gatsby / Vue / Nuxt</li>
+          <li>Ruby / Ruby on Rails</li>
+          <li>WordPress / Contentful</li>
+          <li>AWS / GCP / Netlify / Firebase</li>
+          <li>Git / GitHub / CircleCI</li>
+          <li>etc...</li>
+        </ul>
+      </section>
+      <section className="an-subPage">
+        <h2 css={mt50}>Social Media</h2>
+        <ul css={mt20}>
+          <li>GitHub</li>
+          <li>Wantedly</li>
+          <li>Twitter</li>
+        </ul>
+      </section>
     </Layout>
   )
 }
