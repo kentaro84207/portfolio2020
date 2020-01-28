2fdeee20724a3d06f02db187d44d8bc6502512e8
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-01-28 20:46:02 +0900

    Add test contact form (#7)

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { mt50 } from "../styles/util"
 import Layout from "../components/subLayout"
 import SEO from "../components/seo"
 
@@ -7,6 +8,34 @@ const ContactPage = () => {
     <Layout>
       <SEO title="Contact" />
       <h1 className="an-subPage">Contact</h1>
+      <form css={mt50} name="Contact Form" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="Contact Form" />
+        <p>
+          <label htmlFor="name">
+            Your Name:
+            {' '}
+            <input id="name" type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="email">
+            Your Email:
+            {' '}
+            <input id="email" type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="message">
+            Message:
+            {' '}
+            <textarea id="message" name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      ,
     </Layout>
   )
 }
