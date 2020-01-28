9c035dac1f878c300559bcf30a3637eddd611183
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-01-28 21:37:02 +0900

    Add contact form design (#8)

---
+++ src/pages/thanks.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { css } from '@emotion/core'
+import Layout from "../components/subLayout"
+import SEO from "../components/seo"
+
+const container = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  minHeight: 'calc(100vh - 70px)',
+})
+
+const h1 = css({
+  marginBottom: '30px',
+  marginRight: '100px',
+})
+
+const ThanksPage = () => {
+  return (
+    <Layout>
+      <SEO title="Thanks" />
+      <div css={container}>
+        <h1 css={h1}>Thank you for your inquiry !!</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default ThanksPage
